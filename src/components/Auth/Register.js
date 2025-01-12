53a0566a52e6812ab4a0e71e7a23db26bf050fc8
JustFino <86462499+QuentinRemond@users.noreply.github.com>
2025-01-12 13:22:27 +0100

    Update Register.js

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
 
 function Register() {
-  return <div>Register Component</div>;
+  const { loginWithRedirect } = useAuth0();
+
+  return (
+    <div>
+      <h1>Register</h1>
+      <button onClick={() => loginWithRedirect({ screen_hint: 'signup' })}>
+        Sign Up
+      </button>
+    </div>
+  );
 }
 
 export default Register;
