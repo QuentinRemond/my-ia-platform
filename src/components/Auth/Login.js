013876ff3a25173e1549c400b79649347cf73510
JustFino <86462499+QuentinRemond@users.noreply.github.com>
2025-01-12 13:22:05 +0100

    Update Login.js

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
 
 function Login() {
-  return <div>Login Component</div>;
+  const { loginWithRedirect } = useAuth0();
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <button onClick={() => loginWithRedirect()}>Log In</button>
+    </div>
+  );
 }
 
 export default Login;
